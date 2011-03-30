4ba18773907531eb9d6cd7a23da33fa9372f87bf
arielthepriest <arielthepriest@gmail.com>
2011-03-30 01:00:15 +0000

    Fixed a few bugs and exceptions. Added some useful debug prints and exception prints. Changed location of "mysql-connector-java.....jar"

--- DimesVis/src/dimesSqlBasics/TestingThings.java
+++ DimesVis/src/dimesSqlBasics/TestingThings.java
@@ -10,27 +10,31 @@ public class TestingThings {
 	 */
 	public static void main(String[] args) {
 		String query = "SELECT City, IP, Hostname, ASNumber "
-				+ "FROM DIMES_PLAYGROUND.IPsTblFull "
-				+ "WHERE Country=\"Israel\" " + "ORDER BY City " + "LIMIT 10;";
+					 + "FROM DIMES_PLAYGROUND.IPsTblFull "
+					 + "WHERE Country=\"Israel\" " + "ORDER BY City " + "LIMIT 10;";
 
-		Connector connector = new Connector();
+		Connector connector = new Connector(5551, "DIMES_PLAYGROUND", "codeLimited", "");
 		connector.connect();
 		ResultSet rs = connector.submitStatement(query);
 		try {
 			System.out.println("City\t\tIP\tHostname\tASnumber");
 			System.out.println("-----------------------------------");
-			while(rs.next()){
-				for (int i=1; i<=4; i++){
+			while ((rs != null) && (rs.next())) {
+				for (int i = 1; i <= 4; i++) {
 					System.out.print(rs.getString(i) + "\t");
-					if (i==1){
+					if (i == 1) {
 						System.out.print("\t");
 					}
 				}
 				System.out.println("");
 			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException @ main()");
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+			// ex.printStackTrace();
 		}
 		connector.closeConnection();
 	}
