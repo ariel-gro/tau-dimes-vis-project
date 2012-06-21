d4c80e27c7e06779f063a186153c8d3128ce72f0
arielthepriest@gmail.com <arielthepriest@gmail.com@a05dc6a4-f324-53cc-ade3-c6ae487c7c08>
2012-06-21 15:53:49 +0000

    upgraded to newer SVN in eclipse

---
+++ trunk/DimesVis/src/dimesSqlBasics/ConnectorAPI.java
@@ -0,0 +1,12 @@
+package dimesSqlBasics;
+
+public class ConnectorAPI
+{
+	private Connector	connector	= null;
+
+	public boolean ConnectorAPI_Connect(int portNumber, String userName,
+			String password, String schema)
+	{
+		return false;
+	}
+}
