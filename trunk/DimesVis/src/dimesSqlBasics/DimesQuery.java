134be0025ffd1dcb4ced563cd8c1fcafd59fa86f
arielthepriest@gmail.com <arielthepriest@gmail.com@a05dc6a4-f324-53cc-ade3-c6ae487c7c08>
2012-07-19 05:55:39 +0000

    Added support for specific date query still need to add a check-box in GUI, to enable/disable this option

--- trunk/DimesVis/src/dimesSqlBasics/DimesQuery.java
+++ trunk/DimesVis/src/dimesSqlBasics/DimesQuery.java
@@ -19,12 +19,12 @@ public class DimesQuery
 	private Vector<String>			tables			= null;
 	private String					srcIp			= null;
 	private String					destIp			= null;
-	private String					date			= null;
+	private int						date			= 0;
 	private DimesQueryTimeOption	timeOpt			= null;
 	private int						limit			= 0;
 	private long					sequenceNum		= 0;
 	
-	DimesQuery(QueryType queryType, String schemaName, String mainTable,  String tracerouteTable, String sourceIP, String date, DimesQueryTimeOption queryTimeOption, int limit)
+	DimesQuery(QueryType queryType, String schemaName, String mainTable,  String tracerouteTable, String sourceIP, int date, DimesQueryTimeOption queryTimeOption, int limit)
 	{
 		this.queryType = queryType;
 		
@@ -46,9 +46,7 @@ public class DimesQuery
 		this.tables.add(resTraceIndex, tracerouteTable);
 	}
 	
-	DimesQuery(QueryType queryType, String schemaName, String mainTable,
-			   String tracerouteTable, String sourceIP, String destIp,
-			   String date, DimesQueryTimeOption queryTimeOption, int limit)
+	DimesQuery(QueryType queryType, String schemaName, String mainTable, String tracerouteTable, String sourceIP, String destIp, int date, DimesQueryTimeOption queryTimeOption, int limit)
 	{
 		this.queryType = queryType;
 		
@@ -108,35 +106,20 @@ public class DimesQuery
 		this.tables.add(resTraceIndex, tracerouteTable);
 	}
 	
-	DimesQuery()
-	{
-		//TODO: fictional data in this constructor - need to create constructor with arguments
-		this.schema    = "dimes_results_2007";
-		this.tables    = new Vector<String>();
-		this.srcIp     = "141.35.186.237";
-		this.date      = "2007";
-		this.timeOpt   = DimesQueryTimeOption.Average;
-		this.limit     = 250;
-		this.queryType = QueryType.MainQuery;
-		
-		this.tables.add(resMainIndex, "raw_res_main_2007");
-		this.tables.add(resTraceIndex, "raw_res_tr_2007");
-	}
-	
 	public String toString()
 	{
 		switch (this.queryType)
 		{
 			case MainQuery:
-				return noDateToString();
-//				if (null == this.date)
-//				{
-//					return noDateToString();
-//				}
-//				else
-//				{
-//					return withDateToString();
-//				}
+				//return noDateToString();
+				if (0 == this.date)
+				{
+					return noDateToString();
+				}
+				else
+				{
+					return withDateToString();
+				}
 			
 			case MainQuerySingleIp:
 				return singleIpToString();
@@ -185,8 +168,34 @@ public class DimesQuery
 	
 	private String withDateToString()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		String query = null;
+		if ((null == this.schema)	|| (null == this.tables)	||
+			(null == this.srcIp)	|| (null == this.timeOpt)	||
+			(0    == this.date)		|| (0    > this.limit))
+		{
+			return null;
+		}
+		
+		query = "SELECT SourceIP, SequenceNum, DestIP, "+this.timeOpt+" "
+			  + "FROM "+this.schema+"."+this.tables.get(resMainIndex)+" "
+			  + "LEFT JOIN "+this.schema+"."+this.tables.get(resTraceIndex)+" "
+			  + "ON ((" +this.schema+"."+this.tables.get(resMainIndex)+".SequenceNum = "+this.schema+"."+this.tables.get(resTraceIndex)+".MainSequenceNum) "
+			  + "AND ("+this.schema+"."+this.tables.get(resMainIndex)+".DestAddress = "+this.schema+"."+this.tables.get(resTraceIndex)+".hopAddress)) "
+			  + "WHERE (("+this.schema+"."+this.tables.get(resMainIndex)+".CommandType ='TRACEROUTE') "
+			  + "AND ("+this.schema+"."+this.tables.get(resMainIndex)+".reachedDest = 1) "
+			  + "AND ("+this.schema+"."+this.tables.get(resMainIndex)+".SourceIP = '"+this.srcIp+"') "
+			  + "AND (DAYOFYEAR("+this.schema+"."+this.tables.get(resMainIndex)+".InsertTime) = "+this.date+"))";
+		
+		if (this.limit != 0)	  
+		{
+			query += " LIMIT "+this.limit+";";
+		}
+		else
+		{
+			query += ";";
+		}
+		
+		return query;
 	}
 
 	private String singleIpToString()
